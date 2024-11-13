97465f483e5bf4b2ea7fd2d498c1eeaff6904b8a
Mizraim Martínez <mizraim.martinez@gmail.com>
2024-11-13 09:37:25 -0600

    add input to find places

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
  
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
 
@@ -12,6 +15,7 @@ import './App.css'
 function App() {
 
 		const [precios, setPrecios] = useState("");
+		const [negocio, setNegocio] = useState("");
 		const [coordenadas, setCoordenadas] = useState("");
 		const token = 'aa165f7a-ed0c-4c15-b6a7-ba3f0f186e4c'
  
@@ -21,13 +25,20 @@ function App() {
 		})
 	}, []);
 	
+
+
+	const onChange = (e) => {
+		setNegocio(e.target.value);
+		console.log("name", negocio);
+	};
+
 	const peticiona = () => {
     
 		axios
 			.get(
 				//` https://api.datos.gob.mx/v1/precio.gasolina.publico`
         
-				`https://www.inegi.org.mx/app/api/denue/v1/consulta/buscar/papeleria/${coordenadas['coords']['latitude']}, ${coordenadas['coords']['longitude']}/2000/${token}`
+				`https://www.inegi.org.mx/app/api/denue/v1/consulta/buscar/${negocio}/${coordenadas['coords']['latitude']}, ${coordenadas['coords']['longitude']}/2000/${token}`
 			)
 			.then((res) => {
 				console.log('data', res.data[0]);
@@ -43,16 +54,30 @@ function App() {
 							? console.log("precios",precios):''
       }
       <Container fluid>
-        	<h1>Buscar restaurantes cerca</h1>
+				<h2>Buscar negocios cerca</h2>
+				
+				<Row>
+				
+					<Col xs={12} sm={12}  md={6} lg={4} xl={4} >
+						<InputGroup className="mb-3">
+						 
+							<Form.Control
+								aria-label="Default"
+								aria-describedby="inputGroup-sizing-default"
+									onChange={onChange}
+							/>
+						</InputGroup>
+		
+					</Col>
+				</Row>
 	 
- 
 					<Row    >
 						{precios
             ? precios.map((el, index) => {
                 
 									return (
-										<Col  md={4} lg={4} sm={12} key={index}>
-											<Card style={{ width: "18rem" }} className="bg-red mt-5"   >
+										<Col xs={12} sm={12}  md={6} lg={4} xl={4} key={index}>
+											<Card style={{ width: "18rem" }} className="bg-red mt-4"   >
 									 
                         <Card.Body>
                        
