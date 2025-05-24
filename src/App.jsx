06e7fe891a66e57229d85fc774353d2958db9623
Mizraim Martínez <mizraim.martinez@gmail.com>
2025-05-23 22:05:22 -0600

    change variables and delete comments and align code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
- 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
@@ -14,8 +13,8 @@ import './App.css'
 
 function App() {
 
-		const [precios, setPrecios] = useState("");
-		const [negocio, setNegocio] = useState("");
+		const [lugares, setlugares] = useState("");
+		const [nomNegocio, setnomNegocio] = useState("");
 		const [coordenadas, setCoordenadas] = useState("");
 		const token = 'aa165f7a-ed0c-4c15-b6a7-ba3f0f186e4c'
  
@@ -24,25 +23,21 @@ function App() {
 			setCoordenadas(position)
 		})
 	}, []);
-	
-
 
 	const onChange = (e) => {
-		setNegocio(e.target.value);
-		console.log("name", negocio);
+		setnomNegocio(e.target.value);
+		console.log("name", nomNegocio);
 	};
 
 	const peticiona = () => {
     
 		axios
 			.get(
-				//` https://api.datos.gob.mx/v1/precio.gasolina.publico`
-        
-				`https://www.inegi.org.mx/app/api/denue/v1/consulta/buscar/${negocio}/${coordenadas['coords']['latitude']}, ${coordenadas['coords']['longitude']}/2000/${token}`
+				`https://www.inegi.org.mx/app/api/denue/v1/consulta/buscar/${nomNegocio}/${coordenadas['coords']['latitude']}, ${coordenadas['coords']['longitude']}/2000/${token}`
 			)
 			.then((res) => {
-				console.log('data', res.data[0]);
-				setPrecios(res.data);			
+				setlugares(res.data);		
+					console.log('data', res );
 			})
 		}
  
@@ -50,66 +45,66 @@ function App() {
     <>
        
       {
-        precios
-							? console.log("precios",precios):''
+        lugares
+					? console.log("lugares", lugares)
+					: ''
       }
       <Container fluid>
 				<h2>Buscar negocios cerca</h2>
 				
 				<Row>
 				
-					<Col xs={12} sm={12}  md={6} lg={4} xl={4} >
-						<InputGroup className="mb-3">
-						 
+					<Col xs={12} sm={12}  md={6} lg={10} xl={10} >
+						<InputGroup className="mb-3">	 
 							<Form.Control
-								aria-label="Default"
-								aria-describedby="inputGroup-sizing-default"
-									onChange={onChange}
+		   					aria-label="Large"
+         			 	aria-describedby="inputGroup-sizing-sm"
+								onChange={onChange}
 							/>
 						</InputGroup>
-		
+						<Button onClick={peticiona}> buscar </Button>
 					</Col>
-				</Row>
-	 
-					<Row    >
-						{precios
-            ? precios.map((el, index) => {
-                
-									return (
-										<Col xs={12} sm={12}  md={6} lg={4} xl={4} key={index}>
-											<Card style={{ width: "18rem" }} className="bg-red mt-4"   >
-									 
-                        <Card.Body>
-                       
-													<Card.Title>{el.Nombre}</Card.Title>
-													<Card.Subtitle>{el.Clase_actividad}</Card.Subtitle>
-													<Card.Text>{el.Telefono}</Card.Text>
-													<Card.Text>{el.Calle} {el.Num_Exterior}</Card.Text>
-													<Card.Text>{el.Latitud}</Card.Text>
-													<Card.Text>{el.Longitud}</Card.Text>
-									 
-												</Card.Body>
-											</Card>
-										</Col>
-									);
-							  })
-						: ""}
-					
-						</Row>
-          
-			</Container>
-			
-			<Container fluid>
-			
-				<Row >
-						<Col  md={4} lg={4} sm={12} > 
-							<Button onClick={peticiona}> buscar </Button>
-						</Col>
-				</Row>
-		
+				</Row>				
 			</Container>
       
 
+	 			<Container fluid>
+					<Row    >
+					{						
+						lugares!="No hay resultados. </br> "&&lugares!=''
+            ?lugares.map((el, index) => {
+                
+								return (
+									<Col xs={12} sm={12}  md={6} lg={4} xl={4} key={index}>
+										<Card style={{ width: "18rem" }} className="bg-red mt-4"   >
+									
+											<Card.Body>
+											
+												<Card.Title>{el.Nombre}</Card.Title>
+												<Card.Subtitle>{el.Clase_actividad}</Card.Subtitle>
+												<Card.Text>{el.Telefono}</Card.Text>
+												<Card.Text>{el.Calle} {el.Num_Exterior}</Card.Text>
+												<Card.Text>{el.Latitud}</Card.Text>
+												<Card.Text>{el.Longitud}</Card.Text>
+									
+											</Card.Body>
+										</Card>
+									</Col>
+								);
+							})
+						: ""}
+								
+				</Row>
+
+				<Row>
+					
+					{lugares == "No hay resultados. </br> " ?
+						<p className='war'>Tu busqueda no generó resultados</p>: ""							
+					}
+				</Row>
+          
+			</Container>
+ 
     </>
   )
 }
